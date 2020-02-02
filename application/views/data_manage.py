95bd2fc8b9d086d5a068a894a68cf6da76e4db41
frey <summeriwiner@gmail.com>
2020-02-02 17:29:20 +0800

    新数据集api

--- application/views/data_manage.py
+++ application/views/data_manage.py
@@ -10,6 +10,8 @@
 
 """
 from flask import Blueprint, request, jsonify, g
+from ..models import Data
+from ..database import db_session
 from .. import login_manager
 
 data_manage = Blueprint('data_manage',__name__)
@@ -17,7 +19,7 @@ data_manage = Blueprint('data_manage',__name__)
 
 @data_manage.route('/items')
 @login_manager.login_required
-def new_user():
+def get_items():
     # 获取所有数据
     data = {
         'items':[
@@ -29,6 +31,31 @@ def new_user():
             { 'id': '006', 'name': '数据集名', 'create_time': '2018-12-05 15:59' },
             { 'id': '007', 'name': '数据集名', 'create_time': '2018-12-05 15:59' }
         ]
+    }
+    return jsonify({'code':20000, 'data':data})
+
+@data_manage.route('/item',methods=['POST'])
+@login_manager.login_required
+def new_item():
+    # 数据库建立数据集信息
+    # 把数据导入mongo
+    # 返回数据集id，和数据集name
+    name = request.json.get('name')
+    describe = request.json.get('describe')
+    fileId = request.json.get('fileId')
+    if not name or not describe or not fileId:
+        return jsonify({'code':00000, 'data':data})
+    user = g.user
+    data_path = name + fileId
+    # 数据导入mongo
+
+    # 在数据库建立数据存储信息
+    data = Data(name,describe,data_path)
+    user.datas.append(data)
+    db_session.add(data)
+    db_session.commit()
+    data = {
+        'item_id': data.id
     }
     return jsonify({'code':20000, 'data':data})
 
