f78ed0bd60c8d9d99f5a6f2dc6c633c3b9391d1d
frey <summeriwiner@gmail.com>
2020-02-02 17:28:45 +0800

    数据集模型

--- application/models.py
+++ application/models.py
@@ -9,18 +9,25 @@
 """
 
 """
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, Table, ForeignKey, DateTime
+from sqlalchemy.orm import relationship
 from .database import Base
 from passlib.apps import custom_app_context as pwd_context
 from itsdangerous import (TimedJSONWebSignatureSerializer
                           as Serializer, BadSignature, SignatureExpired)
 from . import app
+import datetime
 
+user_data = Table('users_datas',Base.metadata,
+    Column('users_id',Integer,ForeignKey('users.id')),
+    Column('datas_id',Integer,ForeignKey('datas.id'))
+)
 class User(Base):
     __tablename__ = 'users'
     id = Column(Integer, primary_key=True)
     username = Column(String(32), index=True, unique=True)
     password_hash = Column(String(64))
+    datas = relationship("Data",secondary=user_data)
 
     def hash_password(self, password):
         self.password_hash = pwd_context.encrypt(password)
@@ -43,4 +50,20 @@ class User(Base):
             return None    # invalid token
         user = User.query.get(data['id'])
         return user
+
+class Data(Base):
+    __tablename__ = 'datas'
+    id = Column(Integer,primary_key=True)
+    name = Column(String(32))
+    describe = Column(String(200))
+    create_time = Column(DateTime, default=datetime.datetime.now)
+    data_type = Column(Integer)
+    data_path = Column(String(32))
+    def __init__(self,name,describe,data_path,data_type=0):
+        self.name =name
+        self.describe=describe
+        self.data_path = data_path
+        self.data_type = data_type
+
+
 
